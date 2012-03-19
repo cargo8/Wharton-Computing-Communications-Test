90904ec8723f568a81c9ede90579cb3a19828f48
Nate Close <closen@seas.upenn.edu>
2012-03-18 22:47:28 -0400

    added src folder, created first test class

---
+++ src/edu/upenn/cis350/CreateNewEventTest.java
@@ -0,0 +1,13 @@
+package edu.upenn.cis350;
+import edu.upenn.cis350.CreateNewEvent;
+import android.test.ActivityInstrumentationTestCase2;
+
+
+public class CreateNewEventTest extends ActivityInstrumentationTestCase2<CreateNewEvent> {
+
+	public CreateNewEventTest() {
+		super("edu.upenn.cis350", CreateNewEvent.class);
+		// TODO Auto-generated constructor stub
+	}
+
+}
