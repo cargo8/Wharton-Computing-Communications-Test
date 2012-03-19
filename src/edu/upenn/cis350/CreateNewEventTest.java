5e509b7c3676ff4ac65f38267608373becf7dac3
Nate Close <closen@seas.upenn.edu>
2012-03-18 23:18:22 -0400

    basic test case for createEvent

--- src/edu/upenn/cis350/CreateNewEventTest.java
+++ src/edu/upenn/cis350/CreateNewEventTest.java
@@ -1,6 +1,8 @@
 package edu.upenn.cis350;
 import edu.upenn.cis350.CreateNewEvent;
+import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
 
 
 public class CreateNewEventTest extends ActivityInstrumentationTestCase2<CreateNewEvent> {
@@ -8,6 +10,28 @@ public class CreateNewEventTest extends ActivityInstrumentationTestCase2<CreateN
 	public CreateNewEventTest() {
 		super("edu.upenn.cis350", CreateNewEvent.class);
 		// TODO Auto-generated constructor stub
+	}
+	
+	private Button submitButton;
+	private Activity activity;
+	
+	
+	public void setUp() throws Exception{
+		super.setUp();
+		activity = getActivity();
+		submitButton = (Button)activity.findViewById(R.id.eventButton);
+	}
+	
+	public void testSubmitEmpty(){
+		
+		activity.runOnUiThread(new Runnable() {
+			public void run() {
+				assertNotNull(submitButton);
+				boolean result = submitButton.performClick();
+				assertTrue(result);
+			}
+		});
+		
 	}
 
 }
