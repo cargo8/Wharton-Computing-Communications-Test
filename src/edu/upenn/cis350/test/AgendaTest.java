82fb3c6475781fc0971de3c54178596c6689a799
Arda Kuyumcu <kuyumcu@seas.upenn.edu>
2012-05-04 00:19:13 -0400

    contactlist tests

--- src/edu/upenn/cis350/test/AgendaTest.java
+++ src/edu/upenn/cis350/test/AgendaTest.java
@@ -1,8 +1,15 @@
 package edu.upenn.cis350.test;
 
+import com.parse.ParseQuery;
+
 import edu.upenn.cis350.Agenda;
+import edu.upenn.cis350.EditProfile;
+import edu.upenn.cis350.ManageGroups;
+import edu.upenn.cis350.Agenda.*;
+import android.app.Activity;
+import android.app.Instrumentation.ActivityMonitor;
 import android.test.ActivityInstrumentationTestCase2;
-import android.view.View;
+import android.view.KeyEvent;
 import android.widget.ListView;
 import edu.upenn.cis350.R;
 
@@ -22,16 +29,37 @@ public class AgendaTest extends ActivityInstrumentationTestCase2<Agenda> {
         lv = (ListView) aActivity.findViewById(R.id.eventList);
     }
 	
-	public void testEventClick(){
-		aActivity.runOnUiThread(new Runnable(){
-			public void run(){
-				
-			}
-		});
-		
-		getInstrumentation().waitForIdleSync();
+	public void testGetQuery(){
+		ParseQuery pq = aActivity.getQuery(Filter.ALL);
+		assertNotNull(pq);
 	}
 	
+	public void testRefresh(){
+		ActivityMonitor am = getInstrumentation().addMonitor(Agenda.class.getName(), null, false);
+		getInstrumentation().sendKeyDownUpSync(KeyEvent.KEYCODE_MENU);
+		getInstrumentation().invokeMenuActionSync(aActivity, R.id.refreshAgenda, 0);
+		Activity a = getInstrumentation().waitForMonitorWithTimeout(am, 1000);
+		assertEquals(true, getInstrumentation().checkMonitorHit(am, 1));
+		a.finish();
+	}
+	
+	public void testShowAll(){
+		ActivityMonitor am = getInstrumentation().addMonitor(Agenda.class.getName(), null, false);
+		getInstrumentation().sendKeyDownUpSync(KeyEvent.KEYCODE_MENU);
+		getInstrumentation().invokeMenuActionSync(aActivity, R.id.showAllEvents, 0);
+		Activity a = getInstrumentation().waitForMonitorWithTimeout(am, 1000);
+		assertEquals(true, getInstrumentation().checkMonitorHit(am, 1));
+		a.finish();
+	}
+	
+	public void testShowPastWeek(){
+		ActivityMonitor am = getInstrumentation().addMonitor(Agenda.class.getName(), null, false);
+		getInstrumentation().sendKeyDownUpSync(KeyEvent.KEYCODE_MENU);
+		getInstrumentation().invokeMenuActionSync(aActivity, R.id.showOneWeekOldEvents, 0);
+		Activity a = getInstrumentation().waitForMonitorWithTimeout(am, 1000);
+		assertEquals(true, getInstrumentation().checkMonitorHit(am, 1));
+		a.finish();
+	}
 	
 	
 }
--- src/edu/upenn/cis350/test/ContactListTest.java
+++ src/edu/upenn/cis350/test/ContactListTest.java
@@ -1,7 +1,11 @@
 package edu.upenn.cis350.test;
 
 import edu.upenn.cis350.ContactList;
+import edu.upenn.cis350.R;
+import android.app.Activity;
+import android.app.Instrumentation.ActivityMonitor;
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.KeyEvent;
 import android.widget.ListView;
 
 public class ContactListTest extends ActivityInstrumentationTestCase2<ContactList>{
@@ -17,18 +21,20 @@ public class ContactListTest extends ActivityInstrumentationTestCase2<ContactLis
 	protected void setUp() throws Exception {
 		super.setUp();
         lActivity = this.getActivity();
+        lv = lActivity.getListView();
 	}
 	
-	public void testContactClick(){
-		lActivity.runOnUiThread(new Runnable(){
-			public void run(){
-				
-			}
-		});
-		
-		getInstrumentation().waitForIdleSync();
+	public void testContactCount(){
+		assertNotSame(0, lv.getChildCount()); // static header should make childCount >= 1
 	}
-
 	
-		
+	public void testEditProfile(){
+		ActivityMonitor am = getInstrumentation().addMonitor(ContactList.class.getName(), null, false);
+		getInstrumentation().sendKeyDownUpSync(KeyEvent.KEYCODE_MENU);
+		getInstrumentation().invokeMenuActionSync(lActivity, R.id.refresh, 0);
+		Activity a = getInstrumentation().waitForMonitorWithTimeout(am, 1000);
+		assertEquals(true, getInstrumentation().checkMonitorHit(am, 1));
+		a.finish();
+	}
+	
 }
