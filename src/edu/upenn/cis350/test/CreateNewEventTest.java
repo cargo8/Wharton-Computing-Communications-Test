e1d3cb3826921ac0aa49f76f4b06894c1dc1f96f
Arda Kuyumcu <kuyumcu@seas.upenn.edu>
2012-05-04 05:10:50 -0400

    changes to createneweventtest

--- src/edu/upenn/cis350/test/CreateNewEventTest.java
+++ src/edu/upenn/cis350/test/CreateNewEventTest.java
@@ -2,10 +2,9 @@ package edu.upenn.cis350.test;
 
 import edu.upenn.cis350.CreateNewEvent;
 import edu.upenn.cis350.R;
-import edu.upenn.cis350.R.id;import android.test.ActivityInstrumentationTestCase2;
+import android.test.ActivityInstrumentationTestCase2;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
 
 public class CreateNewEventTest extends ActivityInstrumentationTestCase2<CreateNewEvent> {
 
@@ -13,11 +12,6 @@ public class CreateNewEventTest extends ActivityInstrumentationTestCase2<CreateN
 	private CreateNewEvent cActivity;
 	private EditText eventTitle;
 	private EditText eventDesc;
-	private EditText eventActions;
-	private Button startDate;
-	private Button endDate;
-	private Spinner contact1;
-	private Spinner contact2;
 	
 	public CreateNewEventTest() {
 		super("edu.upenn.cis350", CreateNewEvent.class);
@@ -30,79 +24,44 @@ public class CreateNewEventTest extends ActivityInstrumentationTestCase2<CreateN
 		submitButton = (Button) cActivity.findViewById(R.id.eventButton);
 		eventTitle = (EditText) cActivity.findViewById(R.id.eventTitle);
 		eventDesc = (EditText) cActivity.findViewById(R.id.eventDesc);
-		startDate = (Button) cActivity.findViewById(R.id.pickStartDate);
-		endDate = (Button) cActivity.findViewById(R.id.pickEndDate);
-		contact1 = (Spinner) cActivity.findViewById(R.id.personSpinner1);
-		contact2 = (Spinner) cActivity.findViewById(R.id.personSpinner2);
 	}
 	
 	public void testSubmitEmpty(){
 		cActivity.runOnUiThread(new Runnable(){
 			public void run(){
 				assertNotNull(submitButton);
-//				assertFalse(submitButton.performClick());
+//				assertTrue(submitButton.performClick());
 			}
 		});
 		getInstrumentation().waitForIdleSync();
 	}
 	
-//	public void testSubmitFilled(){
-//		
-//		cActivity.runOnUiThread(new Runnable() {
-//			public void run() {
-//				assertNotNull(eventTitle);
-//				eventTitle.requestFocus();
-//			}
-//		});		
-//		getInstrumentation().waitForIdleSync();
-//		sendKeys("T I T L E 1");
-//		assertEquals(eventTitle.getText().toString(), "title1");
-//		cActivity.runOnUiThread(new Runnable() {
-//			public void run() {
-//				assertNotNull(eventDesc);
-//				eventDesc.requestFocus();
-//			}
-//		});		
-//		getInstrumentation().waitForIdleSync();
-//		sendKeys("D E S C 1");
-//		assertEquals(eventDesc.getText().toString(), "desc1");		
-//		cActivity.runOnUiThread(new Runnable() {
-//			public void run() {
-//				assertNotNull(eventActions);
-//				eventActions.requestFocus();
-//			}
-//		});
-//		getInstrumentation().waitForIdleSync();
-//		sendKeys("A C T I O N S 1");
-//		assertEquals(eventActions.getText().toString(), "actions1");
-//		cActivity.runOnUiThread(new Runnable() {
-//			public void run() {
-//				assertNotNull(contact1);
-//				contact1.requestFocus();
-//				contact1.setSelection(1);
-//			}
-//		});
-//		getInstrumentation().waitForIdleSync();
-//		assertEquals(contact1.getSelectedItem().toString(), "Person 2");
-//		cActivity.runOnUiThread(new Runnable() {
-//			public void run() {
-//				assertNotNull(contact2);
-//				contact2.requestFocus();
-//				contact2.setSelection(3);
-//			}
-//		});
-//		getInstrumentation().waitForIdleSync();
-//		assertEquals(contact2.getSelectedItem().toString(), "Person 4");
-//
-//		cActivity.runOnUiThread(new Runnable() {
-//			public void run() {
-//				assertNotNull(submitButton);
-//				boolean result = submitButton.performClick();
-//				assertTrue(result);
-//			}
-//		});
-//		getInstrumentation().waitForIdleSync();
-//		
-//	}
+	public void testSubmitFilled(){
+		cActivity.runOnUiThread(new Runnable() {
+			public void run() {
+				assertNotNull(eventTitle);
+				eventTitle.requestFocus();
+			}
+		});		
+		sendKeys("T I T L E 1");
+		assertEquals(eventTitle.getText().toString(), "title1");
+		getInstrumentation().waitForIdleSync();
+		cActivity.runOnUiThread(new Runnable() {
+			public void run() {
+				assertNotNull(eventDesc);
+				eventDesc.requestFocus();
+			}
+		});		
+		sendKeys("D E S C 1");
+		assertEquals(eventDesc.getText().toString(), "desc1");
+		getInstrumentation().waitForIdleSync();
+		cActivity.runOnUiThread(new Runnable() {
+			public void run() {
+				assertNotNull(submitButton);
+//				assertTrue(submitButton.performClick());
+			}
+		});
+		getInstrumentation().waitForIdleSync();
+	}
 
 }
