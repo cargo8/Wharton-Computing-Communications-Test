a80a5094832e4fafe4ef8a33580788e4ef0ca6ef
Arda Kuyumcu <kuyumcu@seas.upenn.edu>
2012-05-04 04:59:40 -0400

    more tests

--- src/edu/upenn/cis350/test/LoginTest.java
+++ src/edu/upenn/cis350/test/LoginTest.java
@@ -2,12 +2,16 @@ package edu.upenn.cis350.test;
 
 import android.test.ActivityInstrumentationTestCase2;
 import android.widget.Button;
+import android.widget.EditText;
 import edu.upenn.cis350.Login;
 import edu.upenn.cis350.R;
 
 public class LoginTest extends ActivityInstrumentationTestCase2<Login> {
 
 	private Login lActivity;
+	private Button login;
+	private EditText username;
+	private EditText password;
 	
 	public LoginTest() {
 		super("edu.upenn.cis350", Login.class);
@@ -17,6 +21,9 @@ public class LoginTest extends ActivityInstrumentationTestCase2<Login> {
 	protected void setUp() throws Exception {
 		super.setUp();
         lActivity = this.getActivity();
+        login = (Button) lActivity.findViewById(R.id.loginButton);
+        username = (EditText) lActivity.findViewById(R.id.loginUsername);
+        password = (EditText) lActivity.findViewById(R.id.loginPassword);
 	}
 	
 	public void testRegister(){
@@ -25,6 +32,44 @@ public class LoginTest extends ActivityInstrumentationTestCase2<Login> {
 				Button register = (Button) lActivity.findViewById(R.id.registerButton);
 				assertNotNull(register);
 				assertTrue(register.performClick());
+			}
+		});
+		getInstrumentation().waitForIdleSync();
+	}
+	
+	public void testLoginBlank(){
+		lActivity.runOnUiThread(new Runnable(){
+			public void run(){
+				assertNotNull(login);
+				assertTrue(login.performClick());
+			}
+		});
+		getInstrumentation().waitForIdleSync();
+	}
+	
+	public void testLoginTestUser(){
+		lActivity.runOnUiThread(new Runnable(){
+			public void run(){
+				assertNotNull(username);
+				username.requestFocus();
+			}
+		});
+		sendKeys("T E S T");
+		getInstrumentation().waitForIdleSync();
+		assertEquals("test", username.getText().toString());
+		lActivity.runOnUiThread(new Runnable(){
+			public void run(){
+				assertNotNull(password);
+				password.requestFocus();
+			}
+		});
+		sendKeys("T E S T");
+		getInstrumentation().waitForIdleSync();
+		assertEquals("test", password.getText().toString());
+		lActivity.runOnUiThread(new Runnable(){
+			public void run(){
+				assertNotNull(login.performClick());
+				assertTrue(login.performClick());
 			}
 		});
 		getInstrumentation().waitForIdleSync();
