988ce4bb1794a75715fa9cb82b4d80e1059f407a
Arda Kuyumcu <kuyumcu@seas.upenn.edu>
2012-05-04 05:15:20 -0400

    copied over createnewevent to edit event, mostly same

--- src/edu/upenn/cis350/test/EditEventTest.java
+++ src/edu/upenn/cis350/test/EditEventTest.java
@@ -1,11 +1,17 @@
 package edu.upenn.cis350.test;
 
 import edu.upenn.cis350.EditEvent;
+import edu.upenn.cis350.R;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class EditEventTest extends ActivityInstrumentationTestCase2<EditEvent> {
 
 	private EditEvent eActivity;
+	private Button submitButton;
+	private EditText eventTitle;
+	private EditText eventDesc;
 	
 	public EditEventTest() {
 		super("edu.upenn.cis350", EditEvent.class);
@@ -15,6 +21,46 @@ public class EditEventTest extends ActivityInstrumentationTestCase2<EditEvent> {
 	protected void setUp() throws Exception {
 		super.setUp();
         eActivity = this.getActivity();
+        submitButton = (Button) eActivity.findViewById(R.id.eventButton);
+		eventTitle = (EditText) eActivity.findViewById(R.id.eventTitle);
+		eventDesc = (EditText) eActivity.findViewById(R.id.eventDesc);
 	}
+	
+	public void testSubmitEmpty(){
+		eActivity.runOnUiThread(new Runnable(){
+			public void run(){
+				assertNotNull(submitButton);
+			}
+		});
+		getInstrumentation().waitForIdleSync();
+	}
+	
+	public void testSubmitFilled(){
+		eActivity.runOnUiThread(new Runnable() {
+			public void run() {
+				assertNotNull(eventTitle);
+				eventTitle.requestFocus();
+			}
+		});		
+		sendKeys("T I T L E 1");
+		assertEquals(eventTitle.getText().toString(), "title1");
+		getInstrumentation().waitForIdleSync();
+		eActivity.runOnUiThread(new Runnable() {
+			public void run() {
+				assertNotNull(eventDesc);
+				eventDesc.requestFocus();
+			}
+		});		
+		sendKeys("D E S C 1");
+		assertEquals(eventDesc.getText().toString(), "desc1");
+		getInstrumentation().waitForIdleSync();
+		eActivity.runOnUiThread(new Runnable() {
+			public void run() {
+				assertNotNull(submitButton);
+			}
+		});
+		getInstrumentation().waitForIdleSync();
+	}
+
 
 }
